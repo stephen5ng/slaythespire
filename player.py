40b9a3283a8a27edd61e8518c1ab4e0abb573276
stephen5ng <110236404+stephen5ng@users.noreply.github.com>
2022-08-06 07:42:59 -0700

    extract player.py

---
+++ player.py
@@ -0,0 +1,144 @@
+from __future__ import annotations
+
+import argparse
+import logging
+import math
+import sys
+from collections import namedtuple
+from enum import Enum
+from re import S
+from types import NoneType
+from typing import List, Sequence, Union
+
+import matplotlib.pyplot as plt
+import numpy
+import numpy.polynomial.polynomial as poly
+
+from card import IRONCLAD_STARTER, Card
+
+
+class Player:
+
+    def __init__(self, deck: "Deck", energy: int = 3) -> None:
+        self.deck = deck
+        self.energy = energy
+
+        self.block = 0
+        self.blocks = []
+        self.played_cards = []
+        self.strength = 0
+        self.strength_buff = 0
+        self.post_strength_debuff_once = 0
+
+    @staticmethod
+    def _sort_key(c: Card):
+        pass
+
+    @staticmethod
+    def attack_sort_key(c: Card):
+        k = (c.is_attack(),
+             c.energy if c.energy else 1000,
+             c.exhausts,
+             c.strength_gain,
+             c.strength_multiplier,
+             c.attack)
+        logging.debug(f"attack_sort_key: {c}, {k}")
+        return k
+
+    @staticmethod
+    def defend_sort_key(c: Card):
+        k = (not c.is_attack(),
+             c.energy if c.energy else 1000,
+             c.block)
+        logging.debug(f"defend_sort_key: {c}, {k}")
+        return k
+
+    def select_card_to_play(self, energy) -> Union[Card, None]:
+        for card in self.deck.hand:
+            logging.debug(
+                f"energy: {energy} looking at card: {card} / {self.deck.hand}")
+
+            if card.energy and energy < card.energy:
+                continue
+            return card
+        return None
+
+    def _play_hand(self, monster: Monster):
+        self.deck.sort_hand(self._sort_key)
+
+        energy = self.energy
+        played_cards = []
+
+        card_to_play = self.select_card_to_play(energy)
+        while card_to_play:
+            logging.debug(f"playing card: {card_to_play}")
+            played_cards.append(card_to_play)
+            energy -= card_to_play.energy
+            if card_to_play.exhausts:
+                self.deck.exhaust([card_to_play])
+            else:
+                self.deck.discard([card_to_play])
+
+            if card_to_play.attack:
+                strike_bonus = card_to_play.calculate_strike_bonus(self.deck)
+                damage = (card_to_play.attack_multiplier *
+                          (card_to_play.attack + strike_bonus + self.strength * card_to_play.attack_strength_multiplier))
+                monster.defend(damage)
+
+            if card_to_play.vulnerable:
+                monster.vulnerable(card_to_play.vulnerable)
+
+            self.block += card_to_play.block
+            self.strength_buff += card_to_play.strength_buff
+            self.strength += card_to_play.strength_gain
+            self.post_strength_debuff_once += card_to_play.strength_loss
+            self.strength *= card_to_play.strength_multiplier
+            if card_to_play.draw_card:
+                cards = self.deck.deal(card_to_play.draw_card)
+                logging.info(f"drawing cards: {cards}")
+                self.deck.sort_hand(self._sort_key)
+
+            card_to_play.extra_action(self.deck)
+
+            card_to_play = self.select_card_to_play(energy)
+
+        self.blocks.append(self.block)
+
+        self.deck.discard(self.deck.hand)
+
+        return played_cards
+
+    def play_turn(self, monster: Monster):
+        logging.debug("play_turn...")
+        monster.begin_turn()
+        self.strength += self.strength_buff
+        self.block = 0
+
+        self.deck.deal(5)
+        self.played_cards.append(self._play_hand(monster))
+        logging.info(f"Played: {self.played_cards[-1]}")
+
+        if self.post_strength_debuff_once:
+            self.strength -= self.post_strength_debuff_once
+            self.post_strength_debuff_once = 0
+
+        monster.end_turn()
+        logging.info(f"damage: {monster.get_damage()}")
+
+    def play_game(self, monster: Monster, turns: int):
+        for turn in range(turns):
+            self.play_turn(monster)
+        # logging.info(f"damage: {numpy.cumsum(monster.get_damage())}")
+        # logging.info(f"damage: {monster.get_damage()}")
+
+
+class DefendingPlayer(Player):
+    @staticmethod
+    def _sort_key(c: Card):
+        return (Player.defend_sort_key(c), Player.attack_sort_key(c))
+
+
+class AttackingPlayer(Player):
+    @staticmethod
+    def _sort_key(c: Card):
+        return (Player.attack_sort_key(c), Player.defend_sort_key(c))
--- sts.py
+++ sts.py
@@ -13,6 +13,7 @@ import numpy
 import numpy.polynomial.polynomial as poly
 
 from card import IRONCLAD_STARTER, Card
+from player import Player, AttackingPlayer, DefendingPlayer
 
 logging.basicConfig(filename='sts.log', encoding='utf-8', level=logging.INFO)
 
@@ -133,133 +134,6 @@ class Monster:
         return self._damage
 
 
-class Player:
-
-    def __init__(self, deck: Deck, energy: int = 3) -> None:
-        self.deck = deck
-        self.energy = energy
-
-        self.block = 0
-        self.blocks = []
-        self.played_cards = []
-        self.strength = 0
-        self.strength_buff = 0
-        self.post_strength_debuff_once = 0
-
-    @staticmethod
-    def _sort_key(c: Card):
-        pass
-
-    @staticmethod
-    def attack_sort_key(c: Card):
-        k = (c.is_attack(),
-             c.energy if c.energy else 1000,
-             c.exhausts,
-             c.strength_gain,
-             c.strength_multiplier,
-             c.attack)
-        logging.debug(f"attack_sort_key: {c}, {k}")
-        return k
-
-    @staticmethod
-    def defend_sort_key(c: Card):
-        k = (not c.is_attack(),
-             c.energy if c.energy else 1000,
-             c.block)
-        logging.debug(f"defend_sort_key: {c}, {k}")
-        return k
-
-    def select_card_to_play(self, energy) -> Union[Card, None]:
-        for card in self.deck.hand:
-            logging.debug(
-                f"energy: {energy} looking at card: {card} / {self.deck.hand}")
-
-            if card.energy and energy < card.energy:
-                continue
-            return card
-        return None
-
-    def _play_hand(self, monster: Monster):
-        self.deck.sort_hand(self._sort_key)
-
-        energy = self.energy
-        played_cards = []
-
-        card_to_play = self.select_card_to_play(energy)
-        while card_to_play:
-            logging.debug(f"playing card: {card_to_play}")
-            played_cards.append(card_to_play)
-            energy -= card_to_play.energy
-            if card_to_play.exhausts:
-                self.deck.exhaust([card_to_play])
-            else:
-                self.deck.discard([card_to_play])
-
-            if card_to_play.attack:
-                strike_bonus = card_to_play.calculate_strike_bonus(self.deck)
-                damage = (card_to_play.attack_multiplier *
-                          (card_to_play.attack + strike_bonus + self.strength * card_to_play.attack_strength_multiplier))
-                monster.defend(damage)
-
-            if card_to_play.vulnerable:
-                monster.vulnerable(card_to_play.vulnerable)
-
-            self.block += card_to_play.block
-            self.strength_buff += card_to_play.strength_buff
-            self.strength += card_to_play.strength_gain
-            self.post_strength_debuff_once += card_to_play.strength_loss
-            self.strength *= card_to_play.strength_multiplier
-            if card_to_play.draw_card:
-                cards = self.deck.deal(card_to_play.draw_card)
-                logging.info(f"drawing cards: {cards}")
-                self.deck.sort_hand(self._sort_key)
-
-            card_to_play.extra_action(self.deck)
-
-            card_to_play = self.select_card_to_play(energy)
-
-        self.blocks.append(self.block)
-
-        self.deck.discard(self.deck.hand)
-
-        return played_cards
-
-    def play_turn(self, monster: Monster):
-        logging.debug("play_turn...")
-        monster.begin_turn()
-        self.strength += self.strength_buff
-        self.block = 0
-
-        self.deck.deal(5)
-        self.played_cards.append(self._play_hand(monster))
-        logging.info(f"Played: {self.played_cards[-1]}")
-
-        if self.post_strength_debuff_once:
-            self.strength -= self.post_strength_debuff_once
-            self.post_strength_debuff_once = 0
-
-        monster.end_turn()
-        logging.info(f"damage: {monster.get_damage()}")
-
-    def play_game(self, monster: Monster, turns: int):
-        for turn in range(turns):
-            self.play_turn(monster)
-        # logging.info(f"damage: {numpy.cumsum(monster.get_damage())}")
-        # logging.info(f"damage: {monster.get_damage()}")
-
-
-class DefendingPlayer(Player):
-    @staticmethod
-    def _sort_key(c: Card):
-        return (Player.defend_sort_key(c), Player.attack_sort_key(c))
-
-
-class AttackingPlayer(Player):
-    @staticmethod
-    def _sort_key(c: Card):
-        return (Player.attack_sort_key(c), Player.defend_sort_key(c))
-
-
 def get_frontloaded_damage(damage: list):
     return (damage[0] +
             damage[1]/2.0 +
@@ -321,8 +195,10 @@ def main():
         'cards', nargs='?', help='list of cards (defaults to Ironclad base set)')
     argparser.add_argument(
         '--strategy', help='player strategy (default: AttackingPlayer)', default="AttackingPlayer")
-    argparser.add_argument('--trials', help='number of trials', type=int, default=10000)
-    argparser.add_argument('--turns', help='number of turns', type=int, default=20)
+    argparser.add_argument(
+        '--trials', help='number of trials', type=int, default=10000)
+    argparser.add_argument(
+        '--turns', help='number of turns', type=int, default=20)
     args = argparser.parse_args()
     strategy = eval(args.strategy)
     cards = eval(args.cards)
