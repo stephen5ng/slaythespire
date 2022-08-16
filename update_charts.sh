ca4e7517c5be54a75a6ed358f76ba05ff92eddeb
Stephen Ng <stephen5.ng@gmail.com>
2022-08-15 23:16:43 -0700

    add all links and build_links.sh

--- update_charts.sh
+++ update_charts.sh
@@ -4,3 +4,4 @@ python3 sts.py "[Card.DEFEND]*4 + [Card.STRIKE]*5 + [Card.BASH]" --strategy=Atta
 python3 sts.py "[Card.DEFEND]*4 + [Card.STRIKE]*5 + [Card.BASH] + [Card.DEMON_FORM]" --strategy=AttackingPlayer --trials=10000 --turns=60
 python3 sts.py "[Card.DEFEND]*4 + [Card.STRIKE]*4 + [Card.BASH] + [Card.LIMIT_BREAK_PLUS] + [Card.FLEX]" --trials=1000 --turns=40
 python3 sts.py "[Card.DEFEND]*4 + [Card.STRIKE]*5 + [Card.BASH]" --strategy=AttackingPlayer --trials=1000 --turns=20 --monster=JawWorm   
+
